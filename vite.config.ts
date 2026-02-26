89eb6afc0688a4a2920b1696675b8015e1d385d2
˜marcusnog˜ <marcusnogueira2105@gmail.com>
2026-02-26 17:57:54 -0300

    adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/amnc-tech-com/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
